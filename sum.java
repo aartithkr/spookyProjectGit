361315cc83e7dabdc5669959f03bb584517eafaa
aarti <aarti7772kumari@gmail.com>
2023-03-02 22:08:51 +0530

    third commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x,y,z;
+        System.out.print("enter two number:");
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=x+y;
+        System.out.print("sum="+z);
+    }
+}
